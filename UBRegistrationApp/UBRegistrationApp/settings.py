f9c364509d7c10448dc03763890f1299ca4e4c80
jayners17 <jayners17@gmail.com>
2021-03-23 15:09:31 -0400

    Connected to the gcloud

--- UBRegistrationApp/UBRegistrationApp/settings.py
+++ UBRegistrationApp/UBRegistrationApp/settings.py
@@ -75,12 +75,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'UBRegistrationApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'PORT': '3306',
+        'HOST':'127.0.0.1',
+        'NAME': 'UBRegistrationDB',
+        'USER': 'root',
+        'PASSWORD': '1234',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
