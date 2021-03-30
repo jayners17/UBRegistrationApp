943d986f7cd77cdd538cfcac08e0eacace4511c2
Romeo923 <81250079+Romeo923@users.noreply.github.com>
2021-03-30 14:22:28 -0400

    Update dbDDL.sql

--- UBRegistrationApp/dbDDL.sql
+++ UBRegistrationApp/dbDDL.sql
@@ -5,7 +5,7 @@ CREATE TABLE Course (
   Num_Credits INT(10),
   DName varchar(20),
   PRIMARY KEY (CName),
-  FOREIGN KEY (DName) REFERENCES DEPARTMENT (DName)
+  FOREIGN KEY (DName) REFERENCES Department (DName)
 );
 CREATE TABLE Section (
   CName varchar(20),
@@ -19,8 +19,8 @@ CREATE TABLE Department (
   Name varchar(20),
   Adv_ID_Num INT(10),
   PRIMARY KEY (DName),
-  FOREIGN KEY (Name) REFERENCES COLLEGE (Name),
-  FOREIGN KEY (Adv_ID_Num) REFERENCES ADVISOR (ID_Number)
+  FOREIGN KEY (Name) REFERENCES College (Name),
+  FOREIGN KEY (Adv_ID_Num) REFERENCES Advisor (ID_Number)
 );
 CREATE TABLE College (
   Name varchar(20),
@@ -37,31 +37,31 @@ CREATE TABLE Advisor (
   Name varchar(20),
   DName varchar(20),
   PRIMARY KEY (ID_Number),
-  FOREIGN KEY (ID_Number) REFERENCES LOGIN (ID_Number),
-  FOREIGN KEY (DName) REFERENCES DEPARTMENT (DName)
+  FOREIGN KEY (ID_Number) REFERENCES Login (ID_Number),
+  FOREIGN KEY (DName) REFERENCES Department (DName)
 );
 CREATE TABLE Student (
   ID_Number varchar(20),
   Name varchar(20),
   Adv_ID_Num INT(10),
   PRIMARY KEY (ID_Number),
-  FOREIGN KEY (ID_Number) REFERENCES LOGIN (ID_Number),
-  FOREIGN KEY (Adv_ID_Num) REFERENCES ADVISOR (ID_Number)
+  FOREIGN KEY (ID_Number) REFERENCES Login (ID_Number),
+  FOREIGN KEY (Adv_ID_Num) REFERENCES Advisor (ID_Number)
 );
 CREATE TABLE Admin (
   ID_Number INT(10),
   Name varchar(20),
   PRIMARY KEY (ID_Number),
-  FOREIGN KEY (ID_Number) REFERENCES LOGIN (ID_Number)
+  FOREIGN KEY (ID_Number) REFERENCES Login (ID_Number)
 );
 CREATE TABLE Enrolled (
   St_ID_Num INT(10),
   CName varchar(20),
   SName varchar(20),
   Current_Credits INT(10),
-  FOREIGN KEY (St_ID_Num) REFERENCES STUDENT (ID_Number),
-  FOREIGN KEY (CName) REFERENCES SECTION (CName),
-  FOREIGN KEY (SName) REFERENCES SECTION (SName)
+  FOREIGN KEY (St_ID_Num) REFERENCES Student (ID_Number),
+  FOREIGN KEY (CName) REFERENCES Section (CName),
+  FOREIGN KEY (SName) REFERENCES Section (SName)
 );
 CREATE TABLE Message (
   MEssage_Text TEXT,
@@ -70,30 +70,30 @@ CREATE TABLE Message (
   From_User varchar(20),
   Adv_ID_Num INT(10),
   St_ID_Num INT(10),
-  FOREIGN KEY (Adv_ID_Num) REFERENCES ADVISOR (Adv_ID_Num),
-  FOREIGN KEY (St_ID_Num) REFERENCES STUDENT (ID_Number)
+  FOREIGN KEY (Adv_ID_Num) REFERENCES Advisor (ID_Number),
+  FOREIGN KEY (St_ID_Num) REFERENCES Student (ID_Number)
 );
 CREATE TABLE Graduate (
   ID_Number INT(10),
   Degree_Program varchar(20),
   PRIMARY KEY (ID_Number),
-  FOREIGN KEY (ID_Number)  REFERENCES STUDENT (ID_Number)
+  FOREIGN KEY (ID_Number)  REFERENCES Student (ID_Number)
 );
 CREATE TABLE Undergraduate (
   ID_Number varchar(20),
   Class_Year varchar(20),
   PRIMARY KEY (ID_Number),
-  FOREIGN KEY (ID_Number) REFERENCES STUDENT (ID_Number)
+  FOREIGN KEY (ID_Number) REFERENCES Student (ID_Number)
 );
 CREATE TABLE Majors (
   ID_Number INT(10),
   Major_Name varchar(20),
   PRIMARY KEY (Major_Name),
-  FOREIGN KEY (ID_Number) REFERENCES UNDERGRADUATE (ID_Number)
+  FOREIGN KEY (ID_Number) REFERENCES Undergraduate (ID_Number)
 );
 CREATE TABLE Minors (
   ID_Number INT(10),
   Minor_Name varchar(20),
   PRIMARY KEY (Minor_Name),
-  FOREIGN KEY (ID_Number) REFERENCES UNDERGRADUATE (ID_Number)
+  FOREIGN KEY (ID_Number) REFERENCES Undergraduate (ID_Number)
 );
