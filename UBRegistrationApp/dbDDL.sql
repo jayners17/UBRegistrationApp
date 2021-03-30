eb83155052de585948deac30b375fcdd0e536047
jayners17 <55063854+jayners17@users.noreply.github.com>
2021-03-30 15:34:06 -0400

    Update dbDDL.sql
    Fixed DDL file

--- UBRegistrationApp/dbDDL.sql
+++ UBRegistrationApp/dbDDL.sql
@@ -1,98 +1,97 @@
-CREATE TABLE Course (
-  CName varchar(20),
-  Room_Num INT(10),
-  Professor varchar(20) NOT NULL UNIQUE,
-  Num_Credits INT(10),
-  DName varchar(20),
-  PRIMARY KEY (CName),
-  FOREIGN KEY (DName) REFERENCES Department (DName)
+CREATE TABLE College (
+  Name varchar(20) NOT NULL UNIQUE,
+  PRIMARY KEY (Name)
 );
-CREATE TABLE Section (
-  CName varchar(20),
-  SName varchar(20),
-  Semester varchar(20),
-  Seats_Left INT(10),
-  PRIMARY KEY (SName)
+CREATE TABLE Login (
+  ID_Number INT NOT NULL UNIQUE,
+  Username varchar(20) NOT NULL UNIQUE,
+  Password varchar(20) NOT NULL,
+  PRIMARY KEY (ID_Number)
+);
+CREATE TABLE Advisor (
+  ID_Number INT NOT NULL,
+  Name varchar(20) NOT NULL,
+  DName varchar(20) NOT NULL,
+  PRIMARY KEY (ID_Number),
+  FOREIGN KEY (ID_Number) REFERENCES Login (ID_Number)
 );
 CREATE TABLE Department (
-  DName varchar(20),
-  Name varchar(20),
-  Adv_ID_Num INT(10),
+  DName varchar(20) NOT NULL UNIQUE,
+  Name varchar(20) NOT NULL,
+  Adv_ID_Num INT NOT NULL,
   PRIMARY KEY (DName),
   FOREIGN KEY (Name) REFERENCES College (Name),
   FOREIGN KEY (Adv_ID_Num) REFERENCES Advisor (ID_Number)
 );
-CREATE TABLE College (
-  Name varchar(20),
-  PRIMARY KEY (Name)
-);
-CREATE TABLE Login (
-  ID_Number INT(10),
-  Username varchar(20) NOT NULL UNIQUE,
-  Password varchar(20),
-  PRIMARY KEY (ID_Number)
-);
-CREATE TABLE Advisor (
-  ID_Number INT(10),
-  Name varchar(20),
-  DName varchar(20),
-  PRIMARY KEY (ID_Number),
-  FOREIGN KEY (ID_Number) REFERENCES Login (ID_Number),
+ALTER TABLE Advisor
+ADD FOREIGN KEY (DName) REFERENCES Department (DName);
+CREATE TABLE Course (
+  CName varchar(20) NOT NULL UNIQUE,
+  Room_Num varchar(20),
+  Professor varchar(20) NOT NULL,
+  Num_Credits INT NOT NULL,
+  DName varchar(20) NOT NULL,
+  PRIMARY KEY (CName),
   FOREIGN KEY (DName) REFERENCES Department (DName)
 );
+CREATE TABLE Section (
+  CName varchar(20) NOT NULL,
+  SName varchar(20) NOT NULL UNIQUE,
+  Semester varchar(20) NOT NULL,
+  Seats_Left INT NOT NULL,
+  PRIMARY KEY (SName)
+);
 CREATE TABLE Student (
-  ID_Number varchar(20),
-  Name varchar(20),
-  Adv_ID_Num INT(10),
+  ID_Number INT NOT NULL,
+  Name varchar(20) NOT NULL,
+  Adv_ID_Num INT NOT NULL,
   PRIMARY KEY (ID_Number),
   FOREIGN KEY (ID_Number) REFERENCES Login (ID_Number),
   FOREIGN KEY (Adv_ID_Num) REFERENCES Advisor (ID_Number)
 );
 CREATE TABLE Admin (
-  ID_Number INT(10),
-  Name varchar(20),
+  ID_Number INT NOT NULL,
+  Name varchar(20) NOT NULL,
   PRIMARY KEY (ID_Number),
   FOREIGN KEY (ID_Number) REFERENCES Login (ID_Number)
 );
 CREATE TABLE Enrolled (
-  St_ID_Num INT(10),
-  CName varchar(20),
-  SName varchar(20),
-  Current_Credits INT(10),
+  St_ID_Num INT NOT NULL,
+  CName varchar(20) NOT NULL,
+  SName varchar(20) NOT NULL,
+  Current_Credits INT NOT NULL,
   FOREIGN KEY (St_ID_Num) REFERENCES Student (ID_Number),
-  FOREIGN KEY (CName) REFERENCES Section (CName),
+  FOREIGN KEY (CName) REFERENCES Course (CName),
   FOREIGN KEY (SName) REFERENCES Section (SName)
 );
 CREATE TABLE Message (
   MEssage_Text TEXT,
   Time_Sent DATETIME,
-  To_User varchar(20),
-  From_User varchar(20),
-  Adv_ID_Num INT(10),
-  St_ID_Num INT(10),
-  FOREIGN KEY (Adv_ID_Num) REFERENCES Advisor (ID_Number),
-  FOREIGN KEY (St_ID_Num) REFERENCES Student (ID_Number)
+  To_User varchar(20) NOT NULL,
+  From_User varchar(20) NOT NULL,
+  FOREIGN KEY (To_User) REFERENCES Login (Username),
+  FOREIGN KEY (From_User) REFERENCES Login (Username)
 );
 CREATE TABLE Graduate (
-  ID_Number INT(10),
+  ID_Number INT NOT NULL,
   Degree_Program varchar(20),
   PRIMARY KEY (ID_Number),
   FOREIGN KEY (ID_Number)  REFERENCES Student (ID_Number)
 );
 CREATE TABLE Undergraduate (
-  ID_Number varchar(20),
-  Class_Year varchar(20),
+  ID_Number INT NOT NULL,
+  Class_Year INT,
   PRIMARY KEY (ID_Number),
   FOREIGN KEY (ID_Number) REFERENCES Student (ID_Number)
 );
 CREATE TABLE Majors (
-  ID_Number INT(10),
+  ID_Number INT NOT NULL,
   Major_Name varchar(20),
   PRIMARY KEY (Major_Name),
   FOREIGN KEY (ID_Number) REFERENCES Undergraduate (ID_Number)
 );
 CREATE TABLE Minors (
-  ID_Number INT(10),
+  ID_Number INT NOT NULL,
   Minor_Name varchar(20),
   PRIMARY KEY (Minor_Name),
   FOREIGN KEY (ID_Number) REFERENCES Undergraduate (ID_Number)
