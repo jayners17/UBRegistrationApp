3837dce86fa7102d776fc728e7fda9472d8cb0cb
Romeo923 <81250079+Romeo923@users.noreply.github.com>
2021-04-29 14:17:17 -0400

    Update forms.py

--- UBRegistrationApp/app/forms.py
+++ UBRegistrationApp/app/forms.py
@@ -77,7 +77,9 @@ class SendMessageForm(forms.Form):
     toUser = forms.ModelChoiceField(label='To: ', required=True, queryset=Login.objects.all())
     text = forms.CharField(label='Message', required=True)
     
-
+# class ChangeLoginForm(forms.Form):
+#     choice = forms.ChoiceField(label='Options Menu', choices=CHANGE_MENU, required=True)
+#     courses = forms.ModelChoiceField(label='Courses', required=True, queryset=Section.objects.all())
 
 
 
